500de8cf010499d7767e4d38749b4ce5e93b0a3c
AidrinMorris <aidrinmorris12@gmail.com>
2025-01-25 14:21:22 -0800

    Add files via upload

---
+++ 1-2.py
@@ -0,0 +1,9 @@
+# Prompt the user for a word
+s1 = input("Please enter a word: ")
+
+# Prompt the user for how many times they'd like that word repeated
+s2 = int(input("How many times would you like to repeat that word? "))
+
+# Print the word repeated the specified number of times
+result = s1 * s2
+print(result)
